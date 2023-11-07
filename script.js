0012bd796f59af763937309052765c070697656c
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-07 16:19:54 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sortedBands = bands.sort((a, b) => {
+	const ignoreWords = ['a', 'an', 'the'];
+	const removeIgnoredWords = (str) => str.replace(/^(a|an|the) /i, '');
+	return removeIgnoredWords(a).localeCompare(removeIgnoredWords(b));
+});
+
+const bandList = document.getElementById('bands');
+sortedBands.forEach(article => {
+	const listItem = document.createElement('li');
+	listItem.textContent = article;
+	bandList.appendChild(listItem);
+});
